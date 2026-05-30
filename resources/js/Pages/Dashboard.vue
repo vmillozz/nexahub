b10f720d0e0cb526a19e8793e9e312d252503903
Vito Millozza <vmillozz@gmail.com>
2026-05-30 16:12:22 +0200

    added javascript

---
+++ resources/js/Pages/Dashboard.vue
@@ -0,0 +1,12 @@
+<template>
+  <div style="padding: 2rem; font-family: sans-serif;">
+    <h1>Benvenuto su NexaHub</h1>
+    <p>Ciao, {{ user.name }}</p>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  user: Object,
+})
+</script>
